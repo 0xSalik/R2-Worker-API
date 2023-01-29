30a02162f6d95f082ab4154c13ba64186fdb7c4e
Salik Khan <contact@salikkhan.com>
2023-01-29 09:40:52 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+## WORKER TO INTERACT WITH CLOUDFLARE R2 (S3) BUCKET AS AN API.
+
+This worker acts as an api for your R2 bucket.
+
+## Setup
+
+- Create an authentication key and store it in a KV Namespace with the name `AUTH_KEY_SECRET`. You will use this key to authenticate with your api.
+
+- Edit the `wrangler.toml` file and add your worker name, account id and bucket name to it.
+
+- Install wrangler and sign-in ([Read official docs for instructions](https://developers.cloudflare.com/workers/wrangler/install-and-update/)).
+
+- Deploy the worker with `$ wrangler publish`.
+
+## Using the API
+
+_Replace `<auth-key>` with your actual key (remove the angle brackets), and `example.workers.dev` with your own worker route._
+
+#### Uploading binary data using curl
+
+curl https://example.workers.dev/<location> -X PUT --header 'X-Custom-Auth-Key: <auth-key>' --data-binary 'test'
+
+#### Uploading files using curl
+
+curl https://example.workers.dev/file.ext -X PUT --header 'X-Custom-Auth-Key: <auth-key>' --upload-file ./file.ext
+
+#### Deleting a file using curl
+
+curl https://example.workers.dev/file.ext -X DELETE --header 'X-Custom-Auth-Key: <auth-key>'
+
+That's it. You can just use curl or write an app in a language of your choice to interact with the api.
---
+++ src/index.js
@@ -0,0 +1,57 @@
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    const key = url.pathname.slice(1);
+
+    if (!authorizeRequest(request, env, key)) {
+      return new Response("This is a private content delivery network.", {
+        status: 403,
+      });
+    }
+    switch (request.method) {
+      case "PUT":
+        await env.cdn.put(key, request.body);
+        return new Response(`Put ${key} successfully!`);
+      case "GET":
+        const object = await env.cdn.get(key);
+
+        if (object === null) {
+          return new Response("Object Not Found", { status: 404 });
+        }
+
+        const headers = new Headers();
+        object.writeHttpMetadata(headers);
+        headers.set("etag", object.httpEtag);
+
+        return new Response(object.body, {
+          headers,
+        });
+      case "DELETE":
+        await env.cdn.delete(key);
+        return new Response("Deleted!");
+
+      default:
+        return new Response("Method Not Allowed", {
+          status: 405,
+          headers: {
+            Allow: "PUT, GET, DELETE",
+          },
+        });
+    }
+  },
+};
+// Check requests for a pre-shared secret
+const hasValidHeader = (request, env) => {
+  return request.headers.get("X-Custom-Auth-Key") === env.AUTH_KEY_SECRET;
+};
+function authorizeRequest(request, env, key) {
+  switch (request.method) {
+    case "PUT":
+    case "DELETE":
+      return hasValidHeader(request, env);
+    case "GET":
+      return key;
+    default:
+      return false;
+  }
+}
