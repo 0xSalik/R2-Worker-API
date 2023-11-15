5c06d0a33d14afe0c79f6e5d5c7ed9f9892acb74
Salik Khan <contact@salikkhan.com>
2023-11-15 19:08:59 +0530

    implemented caching

--- src/index.js
+++ src/index.js
@@ -1,47 +1,58 @@
-export default {
-  async fetch(request, env) {
+  async fetch(request, env, context) {
+    
     const url = new URL(request.url);
     const key = url.pathname.slice(1);
-
     if (!authorizeRequest(request, env, key)) {
-      return new Response("This is a private content delivery network.", {
+      return new Response("Unauthorized.", {
         status: 403,
       });
     }
     switch (request.method) {
       case "PUT":
-        await env.bucket.put(key, request.body);
+        await env.cdn.put(key, request.body);
         return new Response(`Put ${key} successfully!`);
       case "GET":
-        const object = await env.bucket.get(key);
-
+        try {
+        const headers = new Headers();
+        const cacheKey = new Request(url.toString(), request);
+        const cache = caches.default;
+        let response = await cache.match(cacheKey);
+        if (response) {
+          console.log(`Cache hit for: ${request.url}.`);
+          return response;
+        }
+        console.log(
+          `Response for request url: ${request.url} not present in cache. Fetching and caching request.`
+        );  
+        const object = await env.cdn.get(key);
         if (object === null) {
           return new Response("Object Not Found", { status: 404 });
         }
-
-        const headers = new Headers();
         object.writeHttpMetadata(headers);
         headers.set("etag", object.httpEtag);
-
-        return new Response(object.body, {
-          headers,
+        headers.append('Cache-Control', 'public, max-age=31536000');
+        response = new Response(object.body, {
+          headers
         });
+        context.waitUntil(cache.put(cacheKey, response.clone()));
+        return response;
+      } catch (e) {
+          return new Response('Error thrown ' + e.message);
+        }
       case "DELETE":
-        await env.bucket.delete(key);
+        await env.cdn.delete(key);
         return new Response("Deleted!");
-
       default:
         return new Response("Method Not Allowed", {
           status: 405,
           headers: {
-            Allow: "PUT, GET, DELETE",
-          },
+            Allow: "PUT, GET, DELETE"
+          }
         });
     }
-  },
+  }
 };
-// Check requests for a pre-shared secret
-const hasValidHeader = (request, env) => {
+var hasValidHeader = (request, env) => {
   return request.headers.get("X-Custom-Auth-Key") === env.AUTH_KEY_SECRET;
 };
 function authorizeRequest(request, env, key) {
@@ -54,4 +65,3 @@ function authorizeRequest(request, env, key) {
     default:
       return false;
   }
-}
