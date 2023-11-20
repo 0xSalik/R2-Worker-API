af3ced3675e018e0955500d9696de356cadfb9bb
Salik <contact@salikkhan.com>
2023-11-20 12:53:28 +0530

    fix a typo

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-  async fetch(request, env, context) {
-    
-    const url = new URL(request.url);
-    const key = url.pathname.slice(1);
-    if (!authorizeRequest(request, env, key)) {
-      return new Response("Unauthorized.", {
-        status: 403,
-      });
-    }
-    switch (request.method) {
-      case "PUT":
-        await env.cdn.put(key, request.body);
-        return new Response(`Put ${key} successfully!`);
-      case "GET":
-        try {
+async fetch(request, env, context) {
+  const url = new URL(request.url);
+  const key = url.pathname.slice(1);
+  if (!authorizeRequest(request, env, key)) {
+    return new Response("Unauthorized.", {
+      status: 403,
+    });
+  }
+  switch (request.method) {
+    case "PUT":
+      await env.bucket.put(key, request.body);
+      return new Response(`Put ${key} successfully!`);
+    case "GET":
+      try {
         const headers = new Headers();
         const cacheKey = new Request(url.toString(), request);
         const cache = caches.default;
@@ -23,35 +22,34 @@
         }
         console.log(
           `Response for request url: ${request.url} not present in cache. Fetching and caching request.`
-        );  
-        const object = await env.cdn.get(key);
+        );
+        const object = await env.bucket.get(key);
         if (object === null) {
           return new Response("Object Not Found", { status: 404 });
         }
         object.writeHttpMetadata(headers);
         headers.set("etag", object.httpEtag);
-        headers.append('Cache-Control', 'public, max-age=31536000');
+        headers.append("Cache-Control", "public, max-age=31536000");
         response = new Response(object.body, {
-          headers
+          headers,
         });
         context.waitUntil(cache.put(cacheKey, response.clone()));
         return response;
       } catch (e) {
-          return new Response('Error thrown ' + e.message);
-        }
-      case "DELETE":
-        await env.cdn.delete(key);
-        return new Response("Deleted!");
-      default:
-        return new Response("Method Not Allowed", {
-          status: 405,
-          headers: {
-            Allow: "PUT, GET, DELETE"
-          }
-        });
-    }
+        return new Response("Error thrown " + e.message);
+      }
+    case "DELETE":
+      await env.bucket.delete(key);
+      return new Response("Deleted!");
+    default:
+      return new Response("Method Not Allowed", {
+        status: 405,
+        headers: {
+          Allow: "PUT, GET, DELETE",
+        },
+      });
   }
-};
+}
 var hasValidHeader = (request, env) => {
   return request.headers.get("X-Custom-Auth-Key") === env.AUTH_KEY_SECRET;
 };
@@ -65,3 +63,4 @@ function authorizeRequest(request, env, key) {
     default:
       return false;
   }
+}
