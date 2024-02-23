d6ac84c1e11455e67cfcafe29c2fd02974f862df
Salik Khan <contact@salikkhan.com>
2024-02-23 13:01:19 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ async fetch(request, env, context) {
         }
         object.writeHttpMetadata(headers);
         headers.set("etag", object.httpEtag);
-        headers.append("Cache-Control", "s-maxage=31536000");
+        headers.append("Cache-Control", "s-maxage=86400");
         response = new Response(object.body, {
           headers,
         });
